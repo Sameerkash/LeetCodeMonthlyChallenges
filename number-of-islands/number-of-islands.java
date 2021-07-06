473500c0207467b92d928967504737a330b61c82
Sameer Kashyap <40424087+Sameerkash@users.noreply.github.com>
2021-07-06 15:00:02 +0530

    Time: 1 ms (99.94%), Space: 41.2 MB (87.74%) - LeetHub

---
+++ number-of-islands/number-of-islands.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+     int count = 0;
+    int m = grid.length;
+    int n = grid[0].length;
+        
+        
+        for(int i = 0; i< m ; i++){
+            for(int j = 0; j< n ; j++){
+                if(grid[i][j] == '1'){
+                    count++;
+                    dfs(i, j , grid, m, n);
+                }        
+            } 
+        }
+        
+        return count;
+    }
+    
+   void dfs(int i , int j , char [][]grid, int m , int n){
+        if( i < 0 || j < 0 ||  i >= m || j >= n  || grid[i][j] == '0') return;
+       
+       if(grid[i][j] == '1')
+          grid[i][j] = '0';
+       
+       dfs(i+1, j, grid, m , n);
+       dfs(i-1, j, grid, m , n);
+       dfs(i, j+1, grid, m , n);
+       dfs(i, j-1, grid, m , n);
+    }
+}
+
